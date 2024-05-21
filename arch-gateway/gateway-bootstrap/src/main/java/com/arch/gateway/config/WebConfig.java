8220905f8c192c16dbcd53dedd41debe4dac499a
chenkun41981 <chenkun41981@hundsun.com>
2024-05-21 08:56:41 +0800

    [效能任务编号]:005 [修改内容说明]: docker配置、跨域解决、打包解决

---
+++ arch-gateway/gateway-bootstrap/src/main/java/com/arch/gateway/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.arch.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web配置 跨域
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // 允许所有来源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
